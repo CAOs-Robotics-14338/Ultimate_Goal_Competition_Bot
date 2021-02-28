247b21f794ac4caa53ee118b309d95dae0f0132b
sparklefbanks <31677932+sparklefbanks@users.noreply.github.com>
2021-02-27 19:09:58 -0800

    Added Concept Program and adjusted logic of AutoDetection Movement

--- FtcRobotController-6.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptTensorFlowObjectDetectionWebcam.java
+++ FtcRobotController-6.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptTensorFlowObjectDetectionWebcam.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
+/* Copyright (c) 2019 FIRST. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted (subject to the limitations in the disclaimer below) provided that
@@ -29,13 +29,10 @@
 
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
@@ -44,16 +41,35 @@ import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 
 import java.util.List;
 
+/**
+ * This 2020-2021 OpMode illustrates the basics of using the TensorFlow Object Detection API to
+ * determine the position of the Ultimate Goal game elements.
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list.
+ *
+ * IMPORTANT: In order to use this OpMode, you need to obtain your own Vuforia license key as
+ * is explained below.
+ */
+@TeleOp(name = "Concept: TensorFlow Object Detection Webcam", group = "Concept")
 
-@Autonomous(name="Detecting Rings and Moving", group="Pushbot")
-
-public class AutoDetectionMovement extends LinearOpMode {
-
-    /* Tensorflow **/
+public class ConceptTensorFlowObjectDetectionWebcam extends LinearOpMode {
     private static final String TFOD_MODEL_ASSET = "UltimateGoal.tflite";
     private static final String LABEL_FIRST_ELEMENT = "Quad";
     private static final String LABEL_SECOND_ELEMENT = "Single";
 
+    /*
+     * IMPORTANT: You need to obtain your own license key to use Vuforia. The string below with which
+     * 'parameters.vuforiaLicenseKey' is initialized is for illustration only, and will not function.
+     * A Vuforia 'Development' license key, can be obtained free of charge from the Vuforia developer
+     * web site at https://developer.vuforia.com/license-manager.
+     *
+     * Vuforia license keys are always 380 characters long, and look as if they contain mostly
+     * random data. As an example, here is a example of a fragment of a valid key:
+     *      ... yIgIzTqZ4mWjk9wd3cZO9T1axEqzuhxoGlfOOI2dRzKS4T0hQ8kT ...
+     * Once you've obtained a license key, copy the string from the Vuforia web site
+     * and paste it in to your code on the next line, between the double quotes.
+     */
     private static final String VUFORIA_KEY =
             "AYDOawL/////AAABmRg/2IBfP0h/gFrTpRMdOcYUlX4rWD72D/Rt+L/Z9YGEQ7REsFBVqq4Yo2hvSJoTrPuVgyHDjjOLgurV9q00YLltcWipqHo1fFxXA45LZHu0ODYKzJ7SCdh/9l9vHtpry3jlefDGdO17owoxqDQMdFwxoAY82mWIm+PhgcKHljKOGXlkCRJnTrEBk4/ldzd6uKw8Y9FMsbNtDlvSW8F2fxPXvhI22mc34D/O0auF3esgHVMq+XND+Ncs6/su+0myu7jiZ7/O8zVFvC5WvuX2P8k8p4RkQQVaNhKerGNGBkmzxHYxJIPKWGwX5NXuO28dIEtZh1N0Bm5BRoSxATCe9DLN41rRufeps6VTC4EwzBC+ ";
 
@@ -69,109 +85,63 @@ public class AutoDetectionMovement extends LinearOpMode {
      */
     private TFObjectDetector tfod;
 
-    /* Declare OpMode members. */
-    private DcMotor FrontRightMotor, FrontLeftMotor, BackRightMotor, BackLeftMotor;
-    HolonomicDrive holonomicDrive;
-
-    private ElapsedTime     runtime = new ElapsedTime();
-
-
-    static final double     FORWARD_SPEED = 0.6;
-    static final double     TURN_SPEED    = 0.5;
-
     @Override
     public void runOpMode() {
-
         // The TFObjectDetector uses the camera frames from the VuforiaLocalizer, so we create that
         // first.
         initVuforia();
         initTfod();
+
         /**
          * Activate TensorFlow Object Detection before we wait for the start command.
          * Do it here so that the Camera Stream window will have the TensorFlow annotations visible.
          **/
         if (tfod != null) {
             tfod.activate();
+
+            // The TensorFlow software will scale the input images from the camera to a lower resolution.
+            // This can result in lower detection accuracy at longer distances (> 55cm or 22").
+            // If your target is at distance greater than 50 cm (20") you can adjust the magnification value
+            // to artificially zoom in to the center of image.  For best results, the "aspectRatio" argument
+            // should be set to the value of the images used to create the TensorFlow Object Detection model
+            // (typically 1.78 or 16/9).
+
+            // Uncomment the following line if you want to adjust the magnification and/or the aspect ratio of the input images.
+            //tfod.setZoom(2.5, 1.78);
         }
-        /*
-         * Initialize the drive system variables.
-         * The init() method of the hardware class does all the work here
-         */
-        FrontRightMotor  = hardwareMap.get(DcMotor.class, "front_right_drive");
-        FrontLeftMotor = hardwareMap.get(DcMotor.class, "front_left_drive");
-        BackRightMotor  = hardwareMap.get(DcMotor.class, "back_right_drive");
-        BackLeftMotor = hardwareMap.get(DcMotor.class, "back_left_drive");
-        holonomicDrive = new HolonomicDrive(FrontRightMotor, FrontLeftMotor, BackRightMotor, BackLeftMotor);
 
-        // Send telemetry message to signify robot waiting;
-        telemetry.addData("Status", "Ready to run");    //
+        /** Wait for the game to begin */
+        telemetry.addData(">", "Press Play to start op mode");
         telemetry.update();
+        waitForStart();
 
-        while(!isStarted()) {
-            if (tfod != null) {
-                // getUpdatedRecognitions() will return null if no new information is available since
-                // the last time that call was made.
-                List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
-                if (updatedRecognitions != null) {
-                    telemetry.addData("# Object Detected", updatedRecognitions.size());
-                    // step through the list of recognitions and display boundary info.
-                    int i = 0;
-                    for (Recognition recognition : updatedRecognitions) {
-                        telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
-                        telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
-                                recognition.getLeft(), recognition.getTop());
-                        telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
-                                recognition.getRight(), recognition.getBottom());
+        if (opModeIsActive()) {
+            while (opModeIsActive()) {
+                if (tfod != null) {
+                    // getUpdatedRecognitions() will return null if no new information is available since
+                    // the last time that call was made.
+                    List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+                    if (updatedRecognitions != null) {
+                        telemetry.addData("# Object Detected", updatedRecognitions.size());
+                        // step through the list of recognitions and display boundary info.
+                        int i = 0;
+                        for (Recognition recognition : updatedRecognitions) {
+                            telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
+                            telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
+                                    recognition.getLeft(), recognition.getTop());
+                            telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
+                                    recognition.getRight(), recognition.getBottom());
+                        }
+                        telemetry.update();
                     }
-                    telemetry.update();
                 }
             }
         }
-        //0-A 1-B 4-C     Ring positions
-        // Wait for the game to start (driver presses PLAY)
-        // waitForStart();
 
-        /**Start of autonomous commands **/
-
-        /* Logic Framwork */
-
-       if (tfod.getUpdatedRecognitions().get(0).getLabel().equals(LABEL_SECOND_ELEMENT) ){ //"Single"
-            telemetry.addData("There is 1 ring detected.", "");
-            telemetry.addData(" Head to the B square, but for now just go straight", "");
-            telemetry.update();
-            holonomicDrive.autoDrive(0,.3 );
-            sleep(2000);
-
-
+        if (tfod != null) {
+            tfod.shutdown();
         }
-        else if (tfod.getUpdatedRecognitions().get(0).getLabel().equals(LABEL_FIRST_ELEMENT)){ //"Quad"
-            telemetry.addData("There are 4 rings detected.", "");
-            telemetry.addData(" Head to the C square, but for now turn right and go straight", "");
-            telemetry.update();
-            holonomicDrive.autoDrive(90, 0.3);
-            sleep(2000);
-            holonomicDrive.autoDrive(0, 0.3);
-            sleep(2000);
-
-        }
-        else if (tfod.getUpdatedRecognitions().size() == 0){ //tfod.getUpdatedRecognitions().size() gives number of objects detected
-            telemetry.addData("There are 0 objects detected.", "");
-            telemetry.addData(" Head to the A square, but for now turn left and go straight", "");
-            telemetry.update();
-            holonomicDrive.autoDrive(180, 0.3);
-            sleep(2000);
-            holonomicDrive.autoDrive(0 ,0.3);
-            sleep(2000);
-
-        }
-        else{
-            telemetry.addData("Error: Fell through entire logic framework", "");
-            telemetry.update();
-
-        }
-
     }
-
 
     /**
      * Initialize the Vuforia localization engine.
