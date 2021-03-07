7af4f3b21c526a145a49d38b1e01f2a3c7fb0b74
ExtraExaByte <encryptedexabyte@gmail.com>
2021-03-06 19:39:05 -0800

    copy and pasting

--- FtcRobotController-6.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WobbleGoal.java
+++ FtcRobotController-6.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WobbleGoal.java
@@ -5,50 +5,56 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class WobbleGoal {
     // Set up Hardware Devices
-        /* Motors */
+    /* Motors */
     DcMotor linSlide;
-    double slidePower = 1.0;
-        /*Servos*/
+    double slideLowerPower = 0.25; //1.0
+    double slideLiftPower = 0.80;
+    /*Servos*/
     Servo claw;
     /*** CLAW Positions ***/
-    double neutral = 0;
-    double grab = 0.5;
-    double store = 1;
+    /**
+     *  double lift = 0;
+     *     double grab = 0.25;
+     *     double stow = 0.5;
+     */
+    double lift = 0;
+    double grab = 0.25;
+    double stow = 0.5;
 
     //Constructors
     public WobbleGoal(DcMotor linearSlide, Servo servo){
         linSlide = linearSlide;
-         claw = servo;
+        claw = servo;
     }
     //Methods
-    /***SERVO METHODS***/
 
+    /***SERVO METHODS***/
     public void setClaw(double pos) {
         this.claw.setPosition(pos);
     }
-
     public void storeClaw(){
-        this.setClaw(store);
+        this.setClaw(stow);
     }
     public void activateClaw() {
         this.setClaw(grab);
     }
-    public void neturalClaw(){
-        this.setClaw(neutral);
+    public void setliftClawPosition(){
+        this.setClaw(lift);
     }
     public void zeroPosition(){
         this.setClaw(0);
     }
+
     /*** LINEAR SLIDE METHODS ***/
     public void raiseWobbleGoal(){
-        linSlide.setPower(slidePower);
+        linSlide.setPower(slideLiftPower);
     }
     public void lowerWobbleGoal(){
-        linSlide.setPower(-1*slidePower);
+        linSlide.setPower(-1*slideLowerPower);
     }
     public void stopGoal(){ linSlide.setPower(0);}
     public void deliverWobbleGoal(){
-        //lower linear slide  timed??
+        //lower linear slide timed, encodered, ....??
         //move claw to its stored position
     }
 }
