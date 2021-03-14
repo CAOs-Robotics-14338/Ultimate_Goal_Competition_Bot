646b455790accc098692bb37947deb5dec4fe264
sparklefbanks <31677932+sparklefbanks@users.noreply.github.com>
2021-03-13 19:56:06 -0800

    Updated vision square to move over the ring placement
    It was a success with the proper line up ;-)

--- FtcRobotController-6.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision.java
+++ FtcRobotController-6.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision.java
@@ -104,7 +104,7 @@ public class Vision extends LinearOpMode
         /*
          * The core values which define the location and size of the sample regions
          */
-        static final Point REGION1_TOPLEFT_ANCHOR_POINT = new Point(181,98); //y:98
+        static final Point REGION1_TOPLEFT_ANCHOR_POINT = new Point(280,115); //x:181 y:98
 
         static final int REGION_WIDTH = 35;
         static final int REGION_HEIGHT = 25;
